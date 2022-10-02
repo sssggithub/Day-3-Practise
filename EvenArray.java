b67e8cf12b05b1c96c9ac0c6bce44359013875a8
sagar <sagarbehera156@gmail.com>
2022-10-02 20:42:27 +0530

    Array in Even Position

---
+++ EvenArray.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class EvenArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] d=new int[] {2,3,4,5,6,7,8};
+for(int s=1;s<d.length;s=s+2) {
+	System.out.println("Even Position of Array:"+d[s]);
+
+}
+	}
+
+}

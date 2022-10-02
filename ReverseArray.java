74906ffb016bf65825bdd8669ca08ea1a4aa39b4
sagar <sagarbehera156@gmail.com>
2022-10-02 20:46:00 +0530

    Reverse in Array

---
+++ ReverseArray.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] f= {1,4,76,86,};
+for (int a=0;a<f.length;a++) {
+	System.out.println(f[a]);
+}
+System.out.println("Reverse Number is:");
+for (int a=f.length-1;a>=0;a--) {
+	System.out.println(f[a]);
+}
+	}
+
+}

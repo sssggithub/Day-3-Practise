247ea7eb6600ca7e394560ff2a97b4e9576c1fe4
sagar <sagarbehera156@gmail.com>
2022-10-02 20:38:09 +0530

    Read Array

---
+++ Arr.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] arr= {1,2,3,5,4};
+for (int a=0;a<arr.length;a++) {
+	System.out.println(arr[a]);
+}
+	}
+
+}
